ce72e9279faadac0e45a4695df91ae2c209d28d6
Sara Freitas <sarafreitas@hotmail.com>
2013-07-25 21:22:46 +0100

    A data object was added and the global variables removed
    Adds an extremelly simple way of accessing data via jQuery plugin
    dynamically making use of the JQuery.data method.

--- jquery.autocomplete.js
+++ jquery.autocomplete.js
@@ -7,30 +7,55 @@
 
         //Merge the user defined options with the default options
         options = $.extend({}, $.fn.AutoComplete.defaultOptions, options);
-
-        //set global variables
-        var m_Textbox = this;
-        var m_TimeoutHandle = 1;
-        var m_Source = options.source;
-        var m_Items = new Array();
-        var m_Index = -1;
+                          
+        //construct a data object that can be called via jQuery.data method
+         var dataFn = {
+             opts: options,           
+                       
+            // Sets the defined option value if it exists
+            set  : function(name, value) {
+                if (this.opts[name]) {
+                    this.opts[name] = value;
+                }
+                
+                // Update the data object for the element
+                this.$el.data('properties', this);
+                
+                // Return the current object which creates method chaining and cleaner code
+                return this;
+            },
+            
+            // Retrieves the value for the defined option
+            get : function(name) {
+                return this.opts[name] || null;
+            },            
+        };
 
         //Loop through each element in the DOM selection
-        return m_Textbox.each(function () {
+         return this.each(function () {
+             // Store the element object in the dataFn object
+             dataFn.$el = $(this);
+             dataFn.items = new Array();
+             dataFn.timeoutHandle = 1;
+             dataFn.index = -1;
+    
+             // Bind the dataFn object to the element
+            dataFn.$el.data('properties', dataFn);
+
             //Keydown Event Listenner
-            $(m_Textbox).on("keydown", function (e) {
+            dataFn.$el.on("keydown", function (e) {
                 var l_CharCode = (e.which) ? e.which : event.keyCode;
 
                 if (l_CharCode != 38 && l_CharCode != 40 & l_CharCode != 13) {
-                    if (m_TimeoutHandle) {
-                        clearTimeout(m_TimeoutHandle);
+                    if (dataFn.timeoutHandle) {
+                        clearTimeout(dataFn.timeoutHandle);
                     }
 
                     //set timeout is needed to create a wait between a user typing and the search method firing
-                    m_TimeoutHandle = window.setTimeout(function () { search(); }, 500); 
+                    dataFn.timeoutHandle = window.setTimeout(function () { search(); }, 500); 
                 }
                 else {
-                    if (m_Textbox.val() != "") {
+                    if (dataFn.$el.val() != "") {
                         if (l_CharCode == 38) { //Arrow up was pressed
                             arrowUp();
                         }
@@ -38,7 +63,7 @@
                              arrowDown();
                         }
                         if (l_CharCode == 13) { //Enter key was pressed, an item will be copied to the input                                                   
-                            var l_Option = $("div.Option", "#AutoComplete").eq(m_Index);
+                            var l_Option = $("div.Option", "#AutoComplete").eq(dataFn.index);
 
                             if (l_Option.length > 0)
                             {
@@ -50,17 +75,18 @@
             })
 
             //When focus out is triggered the autocomplete will be removed from the Dom
-            $(m_Textbox).focusout(function () { window.setTimeout(function () { $("#AutoComplete").remove(); }, 500) });
+            dataFn.$el.focusout(function () { window.setTimeout(function () { $("#AutoComplete").remove(); }, 500) });
            
         });
 
         //this method basically compares the text typed in with the options in the source object,returning the ones that macth
         function search() {
-            var l_Regex = new RegExp($(m_Textbox).val(), "i");
+            var l_Regex = new RegExp(dataFn.$el.val(), "i");
             var l_PlaceHolder = $("<div></div>");
 
-            for (var i = 0; i < m_Source.length ; i++) {
-                var l_Name = m_Source[i];
+            var l_Source = dataFn.opts.source;
+            for (var i = 0; i < l_Source.length ; i++) {
+                var l_Name = l_Source[i];
 
                 if ((l_Name).match(l_Regex)) {
                     var l_Option = $("<div class='Option'>" + l_Name + "</div>")
@@ -79,7 +105,7 @@
             $("#AutoComplete").remove(); //remove any previous AutoComplete added to the page
 
             if (p_Results.children().length > 0) {
-                var l_Position = $(m_Textbox).position(); 
+                var l_Position = dataFn.$el.position(); 
                 
                 var l_AutoComplete = $("<div id='AutoComplete'></div>").css({"display": "inline", "zIndex": options.zIndex });
                 l_AutoComplete.append(p_Results.children());
@@ -95,9 +121,9 @@
         {            
             var l_Item = $("<div class='Item'>" + p_Name + "</div>");
 
-            var l_SearchArea = m_Textbox.parent();
+            var l_SearchArea = dataFn.$el.parent();
 
-            $(l_SearchArea).find(m_Textbox).before(l_Item) //insert the clicked item inside the search area
+            $(l_SearchArea).find(dataFn.$el).before(l_Item) //insert the clicked item inside the search area
 
             l_Item.append($("<div class='Icon'></div>")
                             .width("20")
@@ -108,19 +134,19 @@
                         
             $("#AutoComplete").remove(); //remove AutoComplete
           
-            m_Items.push(p_Name);
+            dataFn.items.push(p_Name);
 
             resizeTextbox();
 
-            m_Textbox.val(""); //clear textbox text
-            m_Textbox.focus();
+            dataFn.$el.val(""); //clear textbox text
+            dataFn.$el.focus();
 
-            m_Index = -1; //clear index
+            dataFn.index = -1; //clear index
         }
 
         //Method called when an item is removed from the input (by clicking in the remove button)
         //1 - remove item from the input
-        //2 - remove item from the global variable m_Items
+        //2 - remove item from the property dataFn.items
         function removeItem(p_Icon)
         {
             var l_Icon= $(p_Icon); //remove button
@@ -131,12 +157,13 @@
 
             var l_Text = l_Item.text();
 
-            //loop through m_Items to remove the one that matches the text of the item clicked 
-            for (var i = 0; i < m_Items.length; i++) {
-                var l_Name = m_Items[i];
+            //loop through dataFn.items to remove the one that matches the text of the item clicked 
+            var l_Items = dataFn.items;
+            for (var i = 0; i < l_Items.length; i++) {
+                var l_Name = l_Items[i];
 
                 if ($.trim(l_Name) == $.trim(l_Text)) {
-                    m_Items.splice(i, 1);
+                   dataFn.items.splice(i, 1);
                 }
             }
 
@@ -144,13 +171,13 @@
 
             resizeTextbox();
 
-            m_Textbox.focus();
+            dataFn.$el.focus();
         }
 
         //Method used to resize the input to fit in the search area, as options are added the space available for the input is smaller.
         function resizeTextbox()
         {
-            var l_SearchArea = m_Textbox.parent();
+            var l_SearchArea = dataFn.$el.parent();
             var l_Items = $("div.Item", l_SearchArea);
 
             var l_LastElem = l_Items.eq(l_Items.length - 1); //access the last element in the search area
@@ -161,38 +188,40 @@
                 var l_Left = l_LastElem.position().left;
                 var l_Width = l_LastElem.width();
 
-                m_Textbox.width(l_SearchAreaW - l_Left - l_Width - 2);
-                m_Textbox.css("margin-top", "-3px");
+                dataFn.$el.width(l_SearchAreaW - l_Left - l_Width - 2);
+                dataFn.$el.css("margin-top", "-3px");
             }
             else {
-                m_Textbox.css("margin-top", "4px"); //default textbox position back to original
-                m_Textbox.width(l_SearchAreaW - 5);
+                dataFn.$el.css("margin-top", "4px"); //default textbox position back to original
+                dataFn.$el.width(l_SearchAreaW - 5);
             }
         }
         //Mehtod called when key arrow down is pressed by the user
         function arrowDown()
         {
             var l_Options = $("div.Option", "#AutoComplete");
+            var l_Index = dataFn.index;
 
-            if (l_Options.length > 0 && m_Index < l_Options.length - 1) {
-                l_Options.eq(m_Index).css("background-color", "");
+            if (l_Options.length > 0 && l_Index < l_Options.length - 1) {
+                l_Options.eq(l_Index).css("background-color", "");
 
-                m_Index += 1;
+                dataFn.index += 1;
 
-                l_Options.eq(m_Index).css("background-color", "#D0D3D6");
+                l_Options.eq(dataFn.index).css("background-color", "#D0D3D6");
             }
         }
 
         //Mehtod called when key arrow up is pressed by the user
         function arrowUp() {
             var l_Options = $("div.Option", "#AutoComplete");
+            var l_Index = dataFn.index;
 
-            if (l_Options.length > 0 && m_Index > 0) {
-                l_Options.eq(m_Index).css("background-color", "");
+            if (l_Options.length > 0 && l_Index > 0) {
+                l_Options.eq(l_Index).css("background-color", "");
 
-                m_Index -= 1;
+                dataFn.index -= 1;
 
-                l_Options.eq(m_Index).css("background-color", "#D0D3D6");
+                l_Options.eq(dataFn.index).css("background-color", "#D0D3D6");
             }
         }     
     }
